db84473c11b8f482eca38dcfee55bd7079a4cc58
Wilson Taylor <wjtaylor03@gmail.com>
2020-09-15 11:56:52 -0700

    Fixed a bug with sed in basics.sh.

--- basics.sh
+++ basics.sh
@@ -9,14 +9,14 @@
 printf "allow-guest=false\n" >> /etc/lightdm/lightdm.conf
 
 # Password history of 5 and length of 8:
-sed -i '/pam_unix\.so/s/$/\tremember=5\tminlength=8' /etc/pam.d/common-password
+sed -i '/pam_unix\.so/s/$/\tremember=5\tminlength=8/' /etc/pam.d/common-password
 # Passwords must be complicated.
-sed -i '/pam_cracklib\.so/s/$/\tucredit=-1\tlcredit=-1\tdcredit=-1\tocredit=-1' /etc/pam.d/common-password
+sed -i '/pam_cracklib\.so/s/$/\tucredit=-1\tlcredit=-1\tdcredit=-1\tocredit=-1/' /etc/pam.d/common-password
 
 # Set password durations.
-sed -i '/^PASS_MAX_DAYS\s*[0-9]+/s/[0-9]+/90'  # Maximum
-sed -i '/^PASS_MIN_DAYS\s*[0-9]+/s/[0-9]+/10'  # Minimum
-sed -i '/^PASS_WARN_AGE\s*[0-9]+/s/[0-9]+/7'  # Days before expiration to warn user.
+sed -i '/^PASS_MAX_DAYS\s*[0-9]+/s/[0-9]+/90/' /etc/login.defs  # Maximum
+sed -i '/^PASS_MIN_DAYS\s*[0-9]+/s/[0-9]+/10/' /etc/login.defs  # Minimum
+sed -i '/^PASS_WARN_AGE\s*[0-9]+/s/[0-9]+/7/' /etc/login.defs  # Days before expiration to warn user.
 
 # account lockout policies.
 # this sets the number of failed login attempts to 5
