6428a88aa12e8819fd77a0f227ac3563c46ff10c
Wilson Taylor <wjtaylor03@gmail.com>
2020-09-29 19:32:27 +0000

    Fixed errors.

--- permissions-stuff.sh
+++ permissions-stuff.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Change the permissions for a bunch of files:
 
 chown root:root /etc/passwd
 chmod 644 /etc/passwd
@@ -19,13 +18,10 @@ chmod u-x,go-wx /etc/group-
 chown root:root /etc/gshadow-
 chown root:shadow /etc/gshadow-
 chmod o-rwx,g-rw /etc/gshadow-
-Configure permissions on /etc/motd
 
 chown root:root /etc/motd
 chmod 644 /etc/motd
-permissions on /etc/issue
 chown root:root /etc/issue
 chmod 644 /etc/issue
-permissions on /etc/issue.net
 chown root:root /etc/issue.net
 chmod 644 /etc/issue.net
